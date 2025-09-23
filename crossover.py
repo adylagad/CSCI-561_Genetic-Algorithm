5fec726bd7cecbfc50f244f770aecdb99a3643a5
adylagad <alagad.usc.edu>
2025-09-22 22:47:46 -0700

    implemented pmx crossover

--- crossover.py
+++ crossover.py
@@ -22,6 +22,32 @@ def orderCrossover(parent1: Tour, parent2: Tour, tourSize: int = 5) -> Tour:
     return child
 
 
+# partially mapped crossover
+def pmxCrossover(parent1: Tour, parent2: Tour, tourSize: int = 5) -> Tour:
+    child: Tour = [(-1, -1, -1)] * len(parent1)
+    positions: List[int] = sample(range(tourSize), 2)
+    startPosition = min(positions)
+    endPosition = max(positions)
+    child[startPosition:endPosition] = parent1[startPosition:endPosition]
+    mapping = {
+        parent2[i]: parent1[i]
+        for i in range(startPosition, endPosition)
+    }
+    for key in mapping:
+        index = parent2.index(key)
+        while child[index][0] != -1:
+            newIndex = parent2.index(parent1[index])
+            index = newIndex
+        child[index] = key
+    i: int = 0
+    for j in range(len(child)):
+        if child[j][0] == -1:
+            while parent2[i] in child:
+                i += 1
+            child[j] = parent2[i]
+    return child
+
+
 # create new population while randomly selecting parents based on roulette wheel and randomly selecting the tour from order corssover
 def crossover(population: Population, populationSize: int = 5) -> Population:
     result: Population = []
