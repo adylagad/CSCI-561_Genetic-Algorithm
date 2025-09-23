f3584c001647f69c0cf1cb9ccc698ce1bd38f00c
adylagad <alagad.usc.edu>
2025-09-22 19:55:22 -0700

    implemented GA with nearest heuristic initial population, order crossover and swap mutation

--- GATypes.py
+++
@@ -1,8 +0,0 @@
-from typing import Tuple, List
-
-City = Tuple[int, int, int]
-Tour = List[Tuple[int, int, int]]
-Population = List[List[Tuple[int, int, int]]]
-Input = Tuple[int, List[Tuple[int, int, int]]]
-FloatList = List[float]
-Parents = Tuple[Tour, Tour]
